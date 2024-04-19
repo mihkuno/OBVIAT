c8a38ab0dab243b396cdf656b9640bad5e11ba81
mihkuno <caindayjoeninyo@gmail.com>
2024-04-19 19:34:07 +0800

    added qr to csv report scheduler

--- detect.py
+++ detect.py
@@ -1,17 +1,155 @@
+import os
 import cv2
+import time
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
 import supervision as sv
 from qreader import QReader
 from ultralytics import YOLO
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+#  Configs
+# ==============================================
+
+output_folder      = 'output'
+camera_index       = 0 # for built-in camera, 1 or 2 for external camera
+weights_path       = './models/n1280/weights/best.pt'
+smtp_username      = "..." # sender email
+smtp_password      = "..." # sender password
+receiver_email     = "..." # recipient email
+subject            = "Student Attendance Report: " # system includes the current time
+body               = "Good day, here is the attached csv file for the report."
+time_of_report     = "17:00"      # report at 5pm 
+pause_duration     = 14 * 60 * 60 # pause for 14 hours
+
+# ==============================================
+
+# Creating Global DataFrame
+attendance = {
+    'Name': [],
+    'Grade': [],
+    'Section': [],
+    'LRN': [],
+    'Time': []
+}
+
+def handleDecodedText(decoded_text):
+    global attendance
+
+    try:
+        # spilt the decoded text    
+        grade_section   = decoded_text.split('\n')[1]
+        student_name    = decoded_text.split('\n')[0]
+        student_grade   = grade_section.split('-')[0].strip()
+        student_section = grade_section.split('-')[1][1:].strip()
+        student_lrn     = decoded_text.split('\n')[2]
+        current_time    = datetime.now().strftime('%H:%M:%S')
+
+        # Append the data to the global DataFrame if not exist
+        if student_lrn not in attendance['LRN']:
+            attendance['Name'].append(student_name)
+            attendance['Grade'].append(student_grade)
+            attendance['Section'].append(student_section)
+            attendance['LRN'].append(student_lrn)
+            attendance['Time'].append(current_time)
+            
+        print('The attendance in memory is: ', attendance)
+    
+    except Exception as e:
+        print(decoded_text)
+        print("QR code is not in the specified format.")
+
+
+def handleScheduledReport():
+    global output_folder
+    global body
+    global subject
+    global attendance
+    global smtp_username
+    global smtp_password
+    global receiver_email
+    global time_of_report
+    global pause_duration
+
+    # create dataframe
+    df = pd.DataFrame(attendance)
+    
+    # create output folder if not exists
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        
+    # Generating file name with current date and time
+    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    file_name = f'student_data_{current_time}.csv'
+    file_path = os.path.join(output_folder, file_name)
+
+    # Export to CSV
+    df.to_csv(file_path, index=False)
+
+    print(f"File saved to: {file_path}")
+
+    # clear the attendance object
+    attendance = {
+        'Name': [],
+        'Grade': [],
+        'Section': [],
+        'LRN': [],
+        'Attendance': []
+    }
+    
+    # Gmail SMTP server configuration
+    sender_email  = smtp_username
+    smtp_server   = "smtp.gmail.com"
+    smtp_port     = 587
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject + current_time # add current time to the subject
+
+    # Attach body to the email
+    message.attach(MIMEText(body, "plain"))
+
+    # Attach CSV file
+    with open(file_path, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {file_name}",
+    )
+    message.attach(part)
+
+    # Connect to Gmail's SMTP server
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  # Secure the connection
+    server.login(smtp_username, smtp_password)  # Login to your Gmail account
+
+    # Send email
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+    # Quit SMTP server
+    server.quit()
+
+    print("Email has been sent successfully!")        
+    
+        
 # Create a QReader instance
 qreader = QReader()
 
 # Load the model
-model = YOLO('./models/n1280/weights/best.pt') 
-# model = YOLO('yolov8n.pt')
+model = YOLO(weights_path) 
 
 # Create a VideoCapture object
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(camera_index)
 
 # Check if the camera is opened successfully
 if not cam.isOpened():
@@ -20,6 +158,18 @@ if not cam.isOpened():
 
 # Capture loop
 while True:
+    
+    # if time is 5pm, save the output file then email it
+    if datetime.now().strftime('%H:%M') == time_of_report:
+        print(f'It is now {time_of_report}, generating student attendance report..')
+        handleScheduledReport()
+            
+        # pause for 14 hours (5pm (17) to 7am = 14 hours)
+        print('\nPausing the program..')
+        cv2.destroyAllWindows()
+        time.sleep(pause_duration)
+        print('\nResuming the program..')        
+    
     # Capture frame-by-frame
     ret, frame = cam.read()
 
@@ -45,21 +195,26 @@ while True:
         annotated_image = sv.BoundingBoxAnnotator().annotate(scene=frame, detections=detections)
         annotated_image = sv.LabelAnnotator().annotate(scene=annotated_image, detections=detections, labels=labels)
 
-        # Extract the first detection region of interest
+        # Extract the first roi detection
         x_min, y_min, x_max, y_max = detections.xyxy[0]
         detect_region = frame[int(y_min):int(y_max), int(x_min):int(x_max)];
 
         # Use the qreader to detect and decode the QR code
         decoded_text = qreader.detect_and_decode(image=detect_region)
 
-        print(decoded_text)
+        if decoded_text:
+            # Extract the first qr detection
+            decoded_text = decoded_text[0] 
+            if decoded_text is not None:
+                handleDecodedText(decoded_text)
 
         cv2.imshow('detections', detect_region)
         cv2.imshow('annotations', annotated_image)
 
     else:
+        black_image = np.zeros((0, 0, 3), dtype=np.uint8)
         cv2.imshow('annotations', frame)    
-        cv2.imshow('detections', (0, 0, 0))
+        cv2.imshow('detections', black_image)
 
     
     # Break the loop if 'q' is pressed
--- extract.py
+++ extract.py
@@ -1,8 +1,44 @@
-import pandas as pd 
+import os
+import pandas as pd
+from datetime import datetime
 
-data=  'Jumalon, Macy Jade O.\n8- RUBY\n462061150055\n'
-student_name = data.split('\n')[0]
-grade_section = data.split('\n')[1]
-student_grade = grade_section.split('-')[0]
-student_section = grade_section.split('-')[1][1:]
-student_lrn = data.split('\n')[2]
+# Data
+data1 = 'Jarold, Macy Dane O.\n8- RUBY\n123456789014\n'
+data2 = 'Doe, John\n9- DIAMOND\n123456789014\n'
+
+# Splitting data1
+grade_section1 = data1.split('\n')[1]
+student_name1 = data1.split('\n')[0]
+student_grade1 = grade_section1.split('-')[0].strip()
+student_section1 = grade_section1.split('-')[1][1:].strip()
+student_lrn1 = data1.split('\n')[2]
+
+# Splitting data2
+grade_section2 = data2.split('\n')[1]
+student_name2 = data2.split('\n')[0]
+student_grade2 = grade_section2.split('-')[0].strip()
+student_section2 = grade_section2.split('-')[1][1:].strip()
+student_lrn2 = data2.split('\n')[2]
+
+# Creating DataFrame
+df = pd.DataFrame({
+    'Name': [student_name1, student_name2],
+    'Grade': [student_grade1, student_grade2],
+    'Section': [student_section1, student_section2],
+    'LRN': [student_lrn1, student_lrn2]
+})
+
+# Creating output folder if not exists
+output_folder = 'output'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Generating file name with current date and time
+current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+file_name = f'student_data_{current_time}.csv'
+file_path = os.path.join(output_folder, file_name)
+
+# Export to CSV
+df.to_csv(file_path, index=False)
+
+print(f"File saved to: {file_path}")
